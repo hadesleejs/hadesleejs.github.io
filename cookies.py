62ad8b888053fc457c7ec2e0ce606c7853b0a0d6
hadeslee <7189444+hadesleejs@users.noreply.github.com>
2018-05-29 10:56:53 +0800

    ogo

--- cookies.py
+++ cookies.py
@@ -25,3 +25,23 @@ https://manage.job910.com/FeedBak/default.aspx?tag=0
 https://manage.job910.com/user/View.aspx?UserID=0
 
 __guid=17726245.802017517894455900.1509584201948.9084; _utmc=5dc1dxtojfn4y1eptkozthpm; _chat_cuid=w7W/X/lo6UHkvhkuQ8yA6g4geqy6VZzTgxFJ/9aahWIgT1uwWGVJNfP+wDpnetxfOvKHHkfBusZDJ6caCVHsAQ==; _chat_cuid.sig=7Dmxp78tWG04YBl13HxRpvdFgrI; Hm_lvt_10fe842eb8d711feaf59d650aee966e0=1527122006,1527235151,1527467973,1527554063; Hm_lpvt_10fe842eb8d711feaf59d650aee966e0=1527554214
+
+
+
+
+POST https://manage.job910.com/login.aspx?callBackUrl=%2fuser%2fView.aspx%3fUserID%3d0 HTTP/1.1
+Host: manage.job910.com
+Connection: keep-alive
+Cache-Control: max-age=0
+Origin: https://manage.job910.com
+Upgrade-Insecure-Requests: 1
+Content-Type: application/x-www-form-urlencoded
+User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+Referer: https://manage.job910.com/login.aspx?callBackUrl=/user/View.aspx?UserID=0
+Accept-Language: zh-CN,zh;q=0.9
+Cookie: __guid=17726245.802017517894455900.1509584201948.9084; _chat_cuid=w7W/X/lo6UHkvhkuQ8yA6g4geqy6VZzTgxFJ/9aahWIgT1uwWGVJNfP+wDpnetxfOvKHHkfBusZDJ6caCVHsAQ==; _chat_cuid.sig=7Dmxp78tWG04YBl13HxRpvdFgrI; Hm_lvt_10fe842eb8d711feaf59d650aee966e0=1527122006,1527235151,1527467973,1527554063; Hm_lpvt_10fe842eb8d711feaf59d650aee966e0=1527554214; ; ASP.NET_SessionId=mbxydmi1sdto3504mhihxolj; _utmc=mbxydmi1sdto3504mhihxolj
+Accept-Encoding: gzip, deflate
+Content-Length: 324
+
+__VIEWSTATE=AfV%2FBAW%2F1yZFKdYDcYhX22B0ezT23uzVFqTAdLTe%2F4GA0vWDNgXDwFowxzczkq7ikyNur1Yz2en1j3%2F%2F8jPveRLfj%2BQ%3D&__VIEWSTATEGENERATOR=C2EE9ABB&__EVENTVALIDATION=njgq1xmIV544Byb2XSI0vxtwzjpDefoeWVVqfgDMsshC%2BmZ6Vi2pOBgcOwmiLxN%2FwFaqmkqrnPVnT8ULEEcQihjbBtPTIzSDoRJnPW00LtICnDfE&btn_login=&UserName=admin&PassWord=admin
